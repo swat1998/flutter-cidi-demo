f4a066139da20c238dc0dd443e71f985de5ad967
Saswat Nayak <saswatnayak@Saswats-MacBook-Air.local>
2026-02-21 23:45:18 +0530

    Adding Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,10 @@ class MyApp extends StatelessWidget {
                 'Built at: $buildTime',
                 style: const TextStyle(color: Colors.white70),
               ),
+              Text(
+                'This is a surprise quiz question! Answer this question to win goodies',
+                style: const TextStyle(color: Colors.white10, fontSize: 20),
+              ),
             ],
           ),
         ),
