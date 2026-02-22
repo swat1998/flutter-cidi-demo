28cda5514c023ef1146b593a1b5f2703addfd669
Saswat Nayak <saswatnayak@Saswats-MacBook-Air.local>
2026-02-22 15:48:35 +0530

    -ve Test case

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text(
-                  '🚀 ResCon\'26',
+                  '🚀 ResCon\'27',
                   style: TextStyle(fontSize: 40, color: Colors.white),
                 ),
                 const SizedBox(height: 20),
--- test/UI_test.dart
+++ test/UI_test.dart
@@ -6,7 +6,7 @@ void main() {
 
   testWidgets('RevCon App renders title, version', (WidgetTester tester) async {
     const testBuildTime = 'TestTime';
-    const testVersion = '1.0.0';
+    const testVersion = '1.0.1';
 
     await tester.pumpWidget(
       const MyApp(buildTime: testBuildTime, version: testVersion),
@@ -15,7 +15,7 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.text('🚀 ResCon\'26'), findsOneWidget);
-    expect(find.text('Version: $testVersion'), findsOneWidget);
-    expect(find.text('Built at: $testBuildTime'), findsOneWidget);
+    expect(find.text('Version: 1.0.1'), findsOneWidget);
+    expect(find.text('Built at: TestTime'), findsOneWidget);
   });
 }
